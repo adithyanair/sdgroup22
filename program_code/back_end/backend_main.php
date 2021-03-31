c549e08f0f63cad82481d2ce1713fa8829c2f74e
shatavaris <70613247+shatavaris@users.noreply.github.com>
2021-03-30 19:18:05 -0500

    Updated Registration with sduserdb

--- program_code/back_end/backend_main.php
+++ program_code/back_end/backend_main.php
@@ -1,37 +1,7 @@
 <?php
 	session_start();
-	
-	// array of mock hardcoded data, WILL DELETE FOR LATER ASSIGNMENTS
-	$users = [
-		"joe" => "123456",
-		"jon" => "654321",
-		"joy" => "abcdef",
-		"shavie" => "1234" 
-	];	
-
-	// login module
-	function loginHandler (&$users) {
-		// init return value
-		$failed = true;
-		if (isset($_POST['username']) && !isset($_SESSION['username'])) {
-			// checks and verifies posted data from login page
-			if (isset($users[$_POST['username']])) {
-				if ($users[$_POST['username']] == $_POST['password']) {
-					$failed = false;
-					$_SESSION['username'] = $_POST['username'];
-					//notifies user of login success
-					echo '<script>alert("Login successful."); 
-								location = "index.php"; </script>';
-				}
-			}
-			// sets the failed login flag
-			if (!isset($_SESSION['username'])) { 
-				//notifies user of login failure
-				echo '<script>alert("Wrong username/password combination.")</script>';
-			}
-		}
-		return $failed;
-	}
+	//connect to database, WILL COMPLETE FOR LATER ASSIGNMENTS
+	$db = mysqli_connect('localhost', 'root', '', 'sduserdb');
 
 	//REGISTER USER
 	function register_handler($db){
@@ -83,51 +53,64 @@
 
 	}
 
-	//connect to database, WILL COMPLETE FOR LATER ASSIGNMENTS
-	$db = mysqli_connect('localhost', 'root', '', 'sduserdb');
-
-	// logic for login module 
-	/*if (isset($_POST['login_user'])) {
-		// call login handler function
-		loginHandler($users);
-	}*/
-
 	//logic for register module
 	if (isset($_POST['reg_user'])) {
 		// call register handler function
 		register_handler($db);
 	}
 
-    // FUTURE CODE FOR REFERENCE - LOGIN USER
-	/*
-	if (isset($_POST['login_user'])) {
-		$username = mysqli_real_escape_string($db, $_POST['username']);
-		$password = mysqli_real_escape_string($db, $_POST['password']);
+	
 
-		if (empty($username)) {
-			array_push($errors, "Username is required");
-		}
-		if (empty($password)) {
-			array_push($errors, "Password is required");
-		}
-
-		if (count($errors) == 0) {
-			$password = md5($password);
-			$query = "SELECT * FROM user WHERE username='$username' AND e_password='$password'";
-			$results = mysqli_query($db, $query);
-
-			if (mysqli_num_rows($results) == 1) {
-				$_SESSION['username'] = $username;
-				$_SESSION['success'] = "You are now logged in";
-				header('location: index.php');
-			}else {
-				array_push($errors, "Wrong username/password combination");
+	// login module
+	function loginHandler ($db) {
+		// init return value
+		/*$failed = true;
+		if (isset($_POST['username']) && !isset($_SESSION['username'])) {
+			// checks and verifies posted data from login page
+			if (isset($users[$_POST['username']])) {
+				if ($users[$_POST['username']] == $_POST['password']) {
+					$failed = false;
+					$_SESSION['username'] = $_POST['username'];
+					//notifies user of login success
+					echo '<script>alert("Login successful."); 
+								location = "index.php"; </script>';
+				}
+			}
+			// sets the failed login flag
+			if (!isset($_SESSION['username'])) { 
+				//notifies user of login failure
+				echo '<script>alert("Wrong username/password combination.")</script>';
 			}
 		}
-	}*/
+		return $failed;*/
+		if (isset($_POST['login_user'])) {
+			$username = mysqli_real_escape_string($db, $_POST['username']);
+			$password = mysqli_real_escape_string($db, $_POST['password']);
+	
+			/*if (empty($username)) {
+				array_push($errors, "Username is required");
+			}
+			if (empty($password)) {
+				array_push($errors, "Password is required");
+			}*/
+				$password = md5($password);
+				$query = "SELECT * FROM user WHERE username='$username' AND password='$password'";
+				$results = mysqli_query($db, $query);
+	
+				if (mysqli_num_rows($results) == 1) {
+					$_SESSION['username'] = $username;
+					$_SESSION['success'] = "You are now logged in";
+					header('location: index.php');
+				}/*else {
+					array_push($errors, "Wrong username/password combination");
+				}*/
+	}
 
-	//REGISTER USER
-	
-	
+	// logic for login module 
+	if (isset($_POST['login_user'])) {
+		// call login handler function
+		loginHandler($db);
+	}
+	}
 
 ?>
