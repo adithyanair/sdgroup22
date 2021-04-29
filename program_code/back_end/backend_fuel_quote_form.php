5f03cdda4440d9f1e95cecb7e004ca23bb804a34
Tyler Hu <46427408+tkhu1@users.noreply.github.com>
2021-04-29 06:32:55 -0500

    ajax

--- program_code/back_end/backend_fuel_quote_form.php
+++ program_code/back_end/backend_fuel_quote_form.php
@@ -3,19 +3,151 @@
 
     //Pricing Module Class
     class pricing_module_class{
-        public $current_price = 1.25; 
-        public $margin = 0; 
-        //Margin =  Current Price * (Location Factor - Rate History Factor + Gallons Requested Factor + Company Profit Factor)
-        function estimatedPrice()
+        //Suggested Price =  Current Price * (Location Factor - Rate History Factor + Gallons Requested Factor + Company Profit Factor)
+      
+//if in texas = 2%, outside of texas = 4%
+        function location_factor($db, $username) //THIS WORKS!
         {
-            //$pricing_mod = $current_price + $margin;
-            return $this -> current_price + $this -> margin; 
+        $location_f = 0;
+        
+        // query to fetch user id
+        $ID_query = "SELECT iduser
+        FROM   user  
+        WHERE  username = '$username' ";
+        $result_ID = mysqli_query($db, $ID_query);
+
+        // error checking
+        if (!$result_ID || mysqli_num_rows($result_ID) == 0) {
+        echo "Could not successfully run query ($ID_query) from DB.";
+        exit;
         }
+
+        // fetches user id from db
+        $value = $result_ID->fetch_object();
+        $ID_user = $value->iduser;
+
+        // query to fetch STATE from the user_info table
+        $profile_query = "SELECT state
+                         FROM   user_info
+                         WHERE  iduser = '$ID_user' ";
+        $result_profile = mysqli_query($db, $profile_query);
+
+        // fetches user profile info from db
+        $row_fetchProfile = mysqli_fetch_assoc($result_profile);
+
+        $state_fetch = $row_fetchProfile["state"];
+
+        if($state_fetch == 'TX'){
+            $location_f = 0.02;
+        }
+        else{
+            $location_f = 0.04;
+        }
+        return $location_f;
+
+        }
+//if client requested fuel before or check query fuel quote table to check if there are any rows for client
+           function ratehistory_factor($db, $username) //WORKS
+           {
+            $ratehistory = 0; 
+
+            $ID_query = "SELECT iduser
+            FROM   user  
+            WHERE  username = '$username' ";
+            $result_ID = mysqli_query($db, $ID_query);
+
+           // error checking
+           if (!$result_ID || mysqli_num_rows($result_ID) == 0) {
+           echo "Could not successfully run query ($ID_query) from DB.";
+           exit;
+           }
+           // fetches user id from db
+           $value = $result_ID->fetch_object();
+           $ID_user = $value->iduser;
+   
+
+           // query to fetch user profile info
+           $userinfo_query = "SELECT iduser_info
+                            FROM   user_info
+                            WHERE  iduser = '$ID_user' ";
+   
+           $result_iduserinfo = mysqli_query($db, $userinfo_query);
+
+           // fetches user profile info from db
+           $value2 = $result_iduserinfo->fetch_object();
+           $ID_userinfo = $value2->iduser_info;
+   
+           //query to fetch fuel quote info 
+           $fuelquote_query = "SELECT * FROM fuel_quote WHERE iduser_info ='$ID_userinfo' LIMIT 1";
+           $results = mysqli_query($db, $fuelquote_query);
+           $count = mysqli_num_rows($results);
+
+            // if count is great than 1, then there is client in the history
+            if ($count) {
+                $ratehistory = 0.01; 
+            }
+            else{
+                $ratehistory = 0;
+                
+            }
+               return $ratehistory; 
+               
+           }
+        
+//2% = above 1000 gallon, 3% if below 1000 gallons
+        function gallonrequested_factor($fuel_fetch) //need work
+        {
+        $gallon_req_factor = 0; 
+        
+            if($fuel_fetch > 1000)
+            {
+                $gallon_req_factor = 0.02; 
+                 /*echo '<script>alert("Welcome '.$fuel_fetch.'!"); 
+                    //location = "fuel_quote_form.php"; </script>';*/
+                //$this -> gallon_req_factor = 0.02;
+                //return $this -> gallon_req_factor;
+            }
+            if($fuel_fetch < 1000) 
+            {
+                $gallon_req_factor = 0.03; 
+               // $this -> gallon_req_factor = 0.03; 
+                //return $this -> gallon_req_factor;
+            }
+               
+            return $gallon_req_factor; 
+        }
+    
+        //For the calculation for margin 
+        function margin_calculation($location , $rate_history , $gallon_req_fac)
+        {
+            $company_profit = 0.1; 
+            $current_price = 1.5;
+            $margin = ($location - $rate_history + $gallon_req_fac +$company_profit) * $current_price;
+
+            
+
+            return $margin; 
+
+        }
+
+        //Margin calculation + current price which is constant
+        function suggestedPrice($margin)
+        {
+             
+            $current_price = 1.5;
+            $pricing_mod = $current_price + $margin;
+            //return $this -> current_price + $this -> margin; 
+
+            return $pricing_mod;
+        
+        }
+
         //Total calculator Function 
-        function totalPrice($ppg, $gallon_req)
+        function totalPrice($pricing_mod, $gallon_req)
         {
-            return $ppg * $gallon_req; 
+            return $pricing_mod * $gallon_req; 
         }
+
     }
 
     // checks if user profile is complete
@@ -83,7 +215,7 @@
 
         // builds the user's whole address to be displayed in quote form
         $user_add_for_form = $row_fetchProfile["client_add1"]."<br>".$row_fetchProfile["client_add2"]."<br>".$row_fetchProfile["city"].", ".$row_fetchProfile["state"].", ".$row_fetchProfile["zipcode"];
-
+        
         return $user_add_for_form;
     }
 
@@ -145,25 +277,37 @@
                 $user_add_for_quote = $row_fetchProfile["client_add1"].", ".$row_fetchProfile["client_add2"].", ".$row_fetchProfile["city"].", ".$row_fetchProfile["state"].", ".$row_fetchProfile["zipcode"];
             }
 
-            //gets estimated prices from pricing module
+            //gets suggested price for pricing module
             $pricing_mod = new pricing_module_class();
-            $estimated_price = $pricing_mod->estimatedPrice();
-            $total_price =  $pricing_mod->totalPrice($estimated_price, $num_gallon);
-
+            $location_f = $pricing_mod->location_factor($db, $username);
+            
+            $ratehistory_f = $pricing_mod->ratehistory_factor($db, $username); //no
+            
+            $gallon_requested_f = $pricing_mod->gallonrequested_factor($num_gallon);
+            
+           
+            $margin = $pricing_mod->margin_calculation($location_f , $ratehistory_f , $gallon_requested_f);
+            $suggestedPrice = $pricing_mod->suggestedPrice($margin);
+            $total_price =  $pricing_mod->totalPrice($suggestedPrice, $num_gallon);
+            
+            //handles ajax quote getter
+            if (isset($_POST['getquote'])) {
+                echo $suggestedPrice;
+                exit;
+            }
+            
             //handles quote submission
             if (isset($_POST['submitquote'])) {
                 //inserts into database
 				$quote_submit_query = "INSERT INTO fuel_quote(del_date, del_add, gallon_req, pricing_mod, total, iduser_info)
-                          	           VALUES ('$date_req', '$user_add_for_quote', '$num_gallon', '$estimated_price', '$total_price', '$ID_user')";
+                          	           VALUES ('$date_req', '$user_add_for_quote', '$num_gallon', '$suggestedPrice', '$total_price', '$ID_user')";
 				$result_insert_quote_query = mysqli_query($db, $quote_submit_query);
                 // error checking
                 if (!$result_insert_quote_query) {
                     echo "Could not successfully run query ($quote_submit_query) from DB.";
                     //exit;
                 }
-                //alerts user of successful quote submission
-                echo '<script>alert("Your quote has been submitted."); 
-                              location = "../main/index.php"; </script>';  
+                exit;
             }
             return true;
         }
--- program_code/fuel_form/fuel_quote_form.php
+++ program_code/fuel_form/fuel_quote_form.php
@@ -20,55 +20,146 @@
 	</div>
 
     <body>
-        <form method="post" action="fuel_quote_form.php" >
+        <form id ="my_form" method="post" action>
             <div class="container">
                 <div class="d-flex">
                     <div class="inputinfo">
                         <label>
                             <span class="inputgallons">Gallons Requested: <span class="required">*</span></span>
-                            <input type="number" step="any" class="form-control" placeholder="Please enter a number" name="gallon_req" min="0" onkeypress="return event.charCode != 45" required><br><br>
+                            <input type="number" step="any" class="form-control" placeholder="Please enter a number" id="id_gr" name="gallon_req" min="0" onkeypress="return event.charCode != 45" required><br><br>
                         </label>
                         <label>
                             <span class="inputdate">Delivery Date: <span class="required">*</span></span>
-                            <input type="text" class="form-control" placeholder="Please enter a date" name="del_date" onfocus="(this.type='date')" required><br><br>
+                            <input type="text" class="form-control" placeholder="Please enter a date" id="id_dd" name="del_date" onfocus="(this.type='date')" required><br><br>
                         </label>
                         <label>
                             <span>Delivering to: </span></span>
                             <!-- calls the user's address from the backend -->
                             <p> <?php echo fetchAddress($db,$user); ?></p>
                         </label>
-                    </div>
+                    </div>      
                     <div class="orderinfo">
                         <table>
                             <tr>
                                 <th colspan="2">Your Quote</th>
                             </tr>
                             <tr>
+                                <td>Delivery Date</td>
+                                <td id='d_date'>N/A</td>
+                            </tr>
+                            <tr>
                                 <td>Gallons Requested</td>
-                                <td>1000.15</td>
+                                <td id='gal_r'>0</td>
                             </tr>
                             <tr>
                                 <td>Price / Gallon</td>
-                                <td>$1.50</td>
-                            </tr>
-                            <tr>
-                                <td>Delivery Date</td>
-                                <td>3/18/21</td>
+                                <td id='ppg'>$0.00</td>
                             </tr>
                             <tr>
                                 <td>Total Amount Due</td>
-                                <td>$1500.23</td>
+                                <td id='total'>$0.00</td>
                             </tr>
                         </table><br>
 
-                        <input type="submit" class="order button" name="getquote" value="Get Quote"><br>
-                        <input type="submit" class="order button" name="submitquote" value="Submit Quote">
+                        <input type="submit" class="order button" id="id_getquote" name="getquote" value="Get Quote"> <br>
+                        <input type="submit" class="order button" id="id_submitquote" name="submitquote" value="Submit Quote" >
+                        
+                        
+                        <script>
+                        function s(){
+                        var i=document.getElementById("gallon_req");
+                        var j=document.getElementById("del_date");
+                        if(i.value=="" || j.value=="")
+                        {
+                            document.getElementById("getquote").disabled=true;
+                            document.getElementById("submitquote").disabled=true;
+                            document.getElementById("getquote").class = "order buttonnn";
+                        }
+                        else
+                        {
+                            document.getElementById("getquote").disabled=false;
+                            document.getElementById("submitquote").disabled=false;
+                        }
+                        </script>
+
                     </div>
                 </div>
             </div>
         </form>
 
         <a href='../main/index.php' style='color:#ffffff; font-size:17px; letter-spacing:1px; text-decoration:none; text-shadow:4px 4px 4px #000000; position:fixed; top:50px; right:100px;">'>Return to Main Menu</a>
+
+        <!-- Script -->
+        <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/additional-methods.min.js"></script>
+        <script>
+        $(document).ready(function(){
+            $('#my_form').validate({
+                showErrors: function(errorMap, errorList) {
+                    // Do nothing here
+                },
+                rules: {
+                    gallon_req: {
+                        required: true,
+                        number: true,
+                        minlength: 0
+                    },
+                    del_date: {
+                        required: true,
+                    }
+                },
+                submitHandler: function(form) {
+                    var isQuote = false;
+                    //behavior for get quote button
+                    $('#id_getquote').click(function(){
+                        var submit_gallon_req = document.getElementById("id_gr").value;
+                        var submit_del_date = document.getElementById("id_dd").value;
+                            //ajax call to get data from backend
+                            jQuery.ajax({
+                                type: 'POST',
+                                url: "../back_end/backend_fuel_quote_form.php",
+                                data: {getquote: 1, gallon_req:submit_gallon_req, del_date:submit_del_date},
+                                success: function(price){
+                                    //updates fields in webpage
+                                    $('#gal_r').text(submit_gallon_req);
+                                    $('#d_date').text(submit_del_date);
+                                    $('#ppg').text('$' + price);
+                                    $('#total').text('$' + price);    //FIX TOTAL VALUE TUPLE?
+                                },
+                                error: function(){
+                                    $('#ppg').text('There is an error in getting quote.');
+                                }
+                            });
+                            isQuote = true;
+                    });
+                    //behavior for submit quote button
+                    $('#id_submitquote').click(function(){
+                        var submit_gallon_req = document.getElementById("id_gr").value;
+                        var submit_del_date = document.getElementById("id_dd").value;
+                        if (isQuote == true) {                              //FIX REGET QUOTE  if new values != old values, fail 
+                            //ajax call to get data from backend            //FIX MULTIREQUESTS
+                            jQuery.ajax({
+                                type: 'POST',
+                                url: "../back_end/backend_fuel_quote_form.php",
+                                data: {submitquote: 1, gallon_req:submit_gallon_req, del_date:submit_del_date},
+                                success: function(price){
+                                    //alerts user and redirects to main menu
+                                    alert("Your quote has been submitted."); 
+                                    location = "../main/index.php";
+                                },
+                                error: function(){
+                                    $('#ppg').text('There is an error in submitting quote.');
+                                }
+                            });
+                            isQuote = false; //resets checker variable to force the user to get a quote first before submitting it
+                        }
+                    });
+                }
+            });
+            
+        });
+        </script>
 
     </body>
 </html>
